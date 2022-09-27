880b062dd9de7467afaed4e3f8e17994c4297d83
Rikuto <c0b211296a@edu.teu.ac.jp>
2022-09-27 17:23:50 +0900

    α　＃1

---
+++ ex01/alf.py
@@ -0,0 +1,20 @@
+import random
+kesu=random.randint(0,5)
+moji=["A","R","D","S","L","F"]
+hyo=[]
+def toi():
+    print(moji)
+    nuki=moji.pop(kesu)
+    print(moji)
+    tou=input()
+    if tou==nuki:
+        print("正解")
+    else:
+        print
+        ("不正解")
+        toi()
+        
+    
+if __name__ == "__main__":
+    toi()
+    
